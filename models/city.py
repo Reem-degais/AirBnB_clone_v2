0e5360216cba5dc3bede7df9af1d25906261af6a
Reem-degais <reem.degais.33@gmail.com>
2023-08-21 10:06:06 -0700

    -

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
 
 
-class City(BaseModel):
+class City(BaseModel, Basei):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    __tablename__ = 'cities'
+
+    state_id = column(string(60), nullable=False, ForeignKey='states.id')
+    name = column(string(128), nullable=False)
