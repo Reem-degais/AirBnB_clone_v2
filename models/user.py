eec6417e592ea564dd3cea5b153c1dee9a533881
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-22 23:44:56 +0300

    Update models/user.py
    - Import Base class from module models.base

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
