ec062e903d63bb95b71e2a58d0d15e923c36d18d
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-23 13:36:06 +0300

    Update models/review.py
    - Add a line before class block to ensure following pycodestyle

--- models/review.py
+++ models/review.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, ForeignKey, String
 
+
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
