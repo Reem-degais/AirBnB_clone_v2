00ac2abd15231284780776f7627877237bde451c
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-23 13:37:57 +0300

    Update models/state.py
    - Ensure line is under 80 characters to follow pycodestyle

--- models/state.py
+++ models/state.py
@@ -11,7 +11,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
         name = ''
 
