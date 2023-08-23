35cab25e46d2d57522685e0a5ec4fa4221edca2b
Reem-degais <reem.degais.33@gmail.com>
2023-08-23 07:57:52 -0700

    -

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A unit test module for the console"""
+
+import unittest
+import json
+import console
+from console import HBNBCommand
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
