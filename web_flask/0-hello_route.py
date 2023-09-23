ffbd369a3a49989e757c9e8b4ea68c8d541b65d8
Reem-degais <reem.degais.33@gmail.com>
2023-09-23 05:21:11 -0700

    fix code_style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """desplay hello hbnb"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     """start Flask and lestin listening on 0.0.0.0, port 5000"""
