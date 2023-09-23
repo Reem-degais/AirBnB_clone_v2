e70d49d62f5b1d303ceb1cc09a228f56c6954c26
Reem-degais <reem.degais.33@gmail.com>
2023-09-23 07:56:26 -0700

    start flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """desplay hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def _hbnb():
+    """display hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """display c followed by the value of the text variable"""
+    text_replaced = text.replace('_', ' ')
+    return "C {}".format(text_replaced)
+
+
+@app.route("/python/", default={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """display python followed by the value of the text variable"""
+    text_replaced = text.replace('_', ' ')
+    return "Python {}".format(text_replaced)
